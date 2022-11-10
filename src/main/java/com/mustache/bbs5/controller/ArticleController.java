d08b2f91917b61ba38217fd77d1e0aaba7e711ab
Hakjun <hiatus9014@gmail.com>
2022-11-10 13:06:32 +0900

    findbyid , list 까지

--- src/main/java/com/mustache/bbs5/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs5/controller/ArticleController.java
@@ -5,9 +5,14 @@ import com.mustache.bbs5.domain.dto.ArticleDto;
 import com.mustache.bbs5.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -19,9 +24,15 @@ public class ArticleController {
     public ArticleController(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
     }
+
     @GetMapping("/new")
     public String createPage() {
         return "new";
+    }
+
+    @GetMapping("")
+    public String index() {
+        return "redirect:/articles/list";
     }
 
     @PostMapping("")
@@ -30,6 +41,26 @@ public class ArticleController {
         log.info(articleDto.getContent()); //log에만 표시
         Article savedArticle = articleRepository.save(articleDto.toEntity());
         log.info("generated:{}",savedArticle.getId());
-        return String.format("redirect:/articles/%d",savedArticle.getId());
+        return "redirect:/articles";
     }
+    @GetMapping("/{id}")
+    public String findbyId(@PathVariable Long id, Model model) {
+        Optional<Article> article = articleRepository.findById(id);
+        if (!article.isEmpty()) {
+            model.addAttribute("article", article.get());
+            return "show";
+        } else {
+            return"error";
+        }
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Article> savedArticle = articleRepository.findAll();
+        model.addAttribute("articles", savedArticle);
+        return "list";
+    }
+
+
+
 }
