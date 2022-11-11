05aff061d88ecc8a396b81852839d20d21544536
Hakjun <hiatus9014@gmail.com>
2022-11-11 13:28:18 +0900

    hospital Controller,repository,test

---
+++ src/main/java/com/mustache/bbs5/domain/Hospital.java
@@ -0,0 +1,25 @@
+package com.mustache.bbs5.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Table(name = "nation_wide_hospitals")//Db에 import한것
+public class Hospital {
+    @Id//
+    private Integer id;
+
+    @Column(name="road_name_address")
+    private String roadNameAddress;
+
+    @Column(name="hospital_name")
+    private String hospitalName;
+    private Integer patientRoomCount;
+    private Integer totalNumberOfBeds;
+    private Float totalAreaSize;
+}
---
+++ src/test/java/com/mustache/bbs5/repository/HospitalRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.mustache.bbs5.repository;
+
+
+import com.mustache.bbs5.domain.Hospital;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class HospitalRepositoryTest {
+
+    @Autowired
+    HospitalRepository hospitalRepository;
+
+    @Test
+    void name(){
+        Optional<Hospital> hospital = hospitalRepository.findById(1);
+        Hospital hp = hospital.get();
+        System.out.println(hp.getId());
+        assertEquals(1, hp.getId());
+
+    }
+}
