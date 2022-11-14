51eff45323c2bf96c9dc62489c576f2cbab3ef95
Hakjun <hiatus9014@gmail.com>
2022-11-14 11:20:11 +0900

    product table작성 yml ddl=update로 수정

---
+++ src/main/java/com/mustache/bbs5/domain/Product.java
@@ -0,0 +1,24 @@
+package com.mustache.bbs5.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="product")//생략가능
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false)
+    private Integer price;
+
+    @Column(nullable=false)
+    private Integer stock;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
