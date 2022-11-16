1915328b66a33d2420e87121978540b2dfb0d330
Hakjun <hiatus9014@gmail.com>
2022-11-16 12:53:07 +0900

    ArticleRestController Test추가

---
+++ src/test/java/com/mustache/bbs5/controller/ArticleRestControllerTest.java
@@ -0,0 +1,46 @@
+package com.mustache.bbs5.controller;
+
+import com.mustache.bbs5.domain.dto.ArticleDto;
+import com.mustache.bbs5.service.ArticleService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ArticleRestControllerTest.class)
+class ArticleRestControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ArticleService articleService;
+
+    @Test
+    @DisplayName("해당 id의 조회기능 테스트")
+    void findSingle() throws Exception {
+        String url = "/api/v1/articles/1";
+        Long id = 1L;
+
+        given(articleService.getArticleById(id)).willReturn(new ArticleDto(1L,"첫번째글","내용입니다"));
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.title").exists())
+                .andExpect(jsonPath("$.content").exists())
+                .andDo(print());
+
+        verify(articleService).getArticleById(id);
+
+    }
+
+}
